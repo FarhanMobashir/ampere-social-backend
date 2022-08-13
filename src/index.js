43824f156be14d95a1870679981e536273738916
Mobashir Farhan <farhanmobashir007@gmail.com>
2022-08-14 00:29:14 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express, { json, urlencoded } from "express";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
@@ -10,7 +10,7 @@ import pinRouter from "./resources/pin/pin.router.js";
 import commentRouter from "./resources/comment/comment.router.js";
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 app.disable("x-powered-by");
 
 app.use(
